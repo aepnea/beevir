2a77dd55529a1903bb7491ba0b2a9a77338ae321
aepnea <cvasquez@zaetachile.cl>
2014-04-10 15:07:31 -0300

    agregue los seed para los usuarios y roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,5 +23,19 @@ Region.create([{nombre: 'Región de Arica y Parinacota', numero: 'XV'}, \
 			   {nombre: 'Región de Los Lagos', numero: 'X'}, \
 			   {nombre: 'Región de Aysén', numero: 'XI'}, \
 			   {nombre: 'Región de Magallanes', numero: 'XII'}])
+# Creando cliente por defecto
 Cliente.create([{razon_social: 'Beevir SpA', nombre: 'Beevir', rut: '15119027-8', direccion: 'German Riesco 430', email: 'contacto@beevir.com', telefono: '72101837', nombre_contacto: 'Cristian Vasquez', region_id: '8'}])
+Cliente.create([{razon_social: '1 Test - Iculo SA', nombre: 'Test-iculo', rut: '15119027-8', direccion: 'German Riesco 430', email: 'contacto@testiculo.com', telefono: '72101837', nombre_contacto: 'Memo Lestaelano', region_id: '10'}])
 
+# Creando usuario dios
+
+user1 = User.create! nombre:'admin', apellido1:'admin', apellido2:'admin', rut:'15119027-8', fono:'72101837', email:"admin@beevir.com", password:'password', cliente_id: '1'
+user1.add_role "admin"
+user1 = User.create! nombre:'copropietario', apellido1:'copropietario', apellido2:'copropietario', rut:'15119027-8', fono:'72101837', email:"copropietario@beevir.com", password:'password', cliente_id: '1'
+user1.add_role "copropietario"
+user1 = User.create! nombre:'copropietario_residente', apellido1:'copropietario_residente', apellido2:'copropietario_residente', rut:'15119027-8', fono:'72101837', email:"copropietario_residente@beevir.com", password:'password', cliente_id: '1'
+user1.add_role "copropietario_residente"
+user1 = User.create! nombre:'arrendatario', apellido1:'arrendatario', apellido2:'arrendatario', rut:'15119027-8', fono:'72101837', email:"arrendatario@beevir.com", password:'password', cliente_id: '1'
+user1.add_role "arrendatario"
+user1 = User.create! nombre:'comite', apellido1:'comite', apellido2:'comite', rut:'15119027-8', fono:'72101837', email:"comite@beevir.com", password:'password', cliente_id: '1'
+user1.add_role "comite"
